a0d8c3ffa3173427023d0c6577c6ec171cfb10ea
Joan J <joanne.javillo@gmail.com>
2021-04-18 21:57:12 +0930

    initial commit for readMe file, trying to make routes work

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # workout_Tracker
+
+
+![License](https://img.shields.io/badge/License-MIT-purple)
+
+## Description 
+- 
+
+ ---
+# Table of Contents 
+
+  - [Description](#Description)
+  - [Technologies](#Technologies)
+  - [Links](#Links)
+  - [Screenshots](#Screenshots)
+  - [License](#License)
+  - [Questions](#questions)
+---
+
+## Technologies
+node js, npm, mongodb, mongoose 
+
+---
+## Links
+ - Github: (https://github.com/joannejavillo/)
+ 
+
+---
+## Screenshots
+![screenshot-of-mainpage](./Assets/images/)
+
+![screenshot-of-image-one](./Assets/images/)
+
+![screenshot-of-image-one](./Assets/images/)
+
+---
+## License
+- MIT
+---
+## Questions
+If you have any questions you can contact me at jo_anne.javillo@outlook.com or you can visit my gitHub page: (https://github.com/joannejavillo)
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,56 @@
 const router = require("express").Router();
+const path = require("path");
 const workOut = require("../models/workout.js");
 
-router.post("/api/workout", ({body}, res) => {
-    workOut.create(body)
-    .then(dbworkOut => {
-        res.json(dbworkOut);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+router.get("/", async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
+router.get("/exercise", async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/stats", async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/workouts", (req, res) => {
+    workOut.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+            },
+        },
+    ])
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workout", ({ body }, res) => {
+    workOut.create(body)
+        .then(dbworkOut => {
+            res.json(dbworkOut);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post
 module.exports = router;
