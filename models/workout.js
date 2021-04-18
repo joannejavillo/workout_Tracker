8d655823e525589af38b0fb25e889e31d27d3cd2
Joan J <joanne.javillo@gmail.com>
2021-04-18 15:13:07 +0930

    added the workout js, change the local port of the server

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: {
-      type: Date,
-      default: () => new Date(),
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        required: "What is the type of the exercise"
+      },
+      name: {
+        type: String,
+        required: "Enter the name of the exercise"
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "Enter the duration of the exercise in minutes "
+      },
+      distance: {
+        type: Number,
+      },
     },
+  ],
 });
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = {Workout};
